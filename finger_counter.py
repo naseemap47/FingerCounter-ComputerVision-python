897a2070634556fc47623798c30045787ecd7569
NASEEM A P <naseemalassampattil@gmail.com>
2022-03-03 20:37:57 +0530

    added fps and printed landmarks

--- finger_counter.py
+++ finger_counter.py
@@ -3,9 +3,26 @@ import mediapipe as mp
 import time
 
 cap = cv2.VideoCapture(0)
+mp_hand = mp.solutions.hands
+hand = mp_hand.Hands(max_num_hands=1)
+
+p_time =0
 
 while True:
     success, img = cap.read()
+
+    img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    result = hand.process(img_rgb)
+    print(result.multi_hand_landmarks)
+
+    c_time = time.time()
+    fps = 1 / (c_time - p_time)
+    p_time = c_time
+    cv2.putText(
+        img, f'FPS: {int(fps)}',
+        (10, 60), cv2.FONT_HERSHEY_PLAIN,
+        3,(255, 0, 0), 3
+    )
 
     cv2.imshow("Image", img)
     cv2.waitKey(1)
