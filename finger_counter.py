acbbfce9850f48aab5ad199ca745f39108788bff
NASEEM A P <naseemalassampattil@gmail.com>
2022-03-03 20:44:07 +0530

    draw hand landmarks, stored id, x, y in a list

--- finger_counter.py
+++ finger_counter.py
@@ -5,15 +5,25 @@ import time
 cap = cv2.VideoCapture(0)
 mp_hand = mp.solutions.hands
 hand = mp_hand.Hands(max_num_hands=1)
+mp_draw = mp.solutions.drawing_utils
 
 p_time =0
 
+hand_landmarks_list = []
 while True:
     success, img = cap.read()
 
     img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     result = hand.process(img_rgb)
-    print(result.multi_hand_landmarks)
+    # print(result.multi_hand_landmarks)
+    if result.multi_hand_landmarks:
+        for hand_lm in result.multi_hand_landmarks:
+            for id, lm in enumerate(hand_lm.landmark):
+                height, width, channel = img.shape
+                x, y = int(lm.x * width), int(lm.y * height)
+                hand_landmarks_list.append([id, x, y])
+                print(hand_landmarks_list)
+            mp_draw.draw_landmarks(img, hand_lm, mp_hand.HAND_CONNECTIONS)
 
     c_time = time.time()
     fps = 1 / (c_time - p_time)
